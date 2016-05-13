14ef8ba843e49b8d7df35fc39c9310f61a902999
Saejin Park <tpwls318@nate.com>
2016-05-13 19:16:15 +0900

    Webhook setting

--- hello/views.py
+++ hello/views.py
@@ -6,6 +6,18 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
+    if request.method == 'GET':
+        # authentication
+        if request.GET['hub.verify_token'] == '12345':
+            # success: response with hub.challenge
+            return HttpResponse(request.GET['hub.challenge'])
+        else:
+            # fail: response with any error message
+            return HttpResponse('Error')
+
+    elif request.method == 'POST':
+        pass
+
     return render(request, 'index.html')
 
 
