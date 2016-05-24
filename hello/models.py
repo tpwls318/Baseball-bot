fb724f0a9d87677f18fe60fb1966788da160f314
Saejin Park <tpwls318@nate.com>
2016-05-24 20:21:08 +0900

    Create player table

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Player(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    position  = models.CharField(max_length = 50)
+    age = models.PositiveSmallIntegerField()
+    team = models.CharField(max_length=50)
+    contract_date = models.DateField(auto_now = True, auto_now_add = False)
